7d3574fcf85b6d4b27797f3d515f5cdc04d84650
Noe <osorionoe@gmail.com>
2018-06-05 18:26:49 -0500

    Menu en proceso

---
+++ EduTest/MenuCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MenuCollectionViewCell.swift
+//  EduTest
+//
+//  Created by Noe Osorio on 05/06/18.
+//  Copyright © 2018 Noe Osorio. All rights reserved.
+//
+
+import UIKit
+
+class MenuCollectionViewCell: UICollectionViewCell {
+    @IBOutlet var image: UIImageView!
+    
+    @IBOutlet var title: UILabel!
+}
